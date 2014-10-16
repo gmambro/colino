b89eb34df9346cffc580fbae0907d0534a67f69f
Gabriele Mambrini <g.mambrini@gmail.com>
2014-10-16 22:05:59 +0200

    added some comments and stubs

--- colino/rule.py
+++ colino/rule.py
@@ -1,20 +1,88 @@
-class Predicate(object):
-
-    def __init__(self, time_constraint=None):
-        self._constraint=None
+class Condition(object):
     
     def evaluate(self, context, event):
         raise NotImplemented
-    
 
+class Node(object):
 
-class Rule(object):
+    def __init__(self, time_limit):
+        self.time_limit = time_limit
+        
+class Edge(object):
+
+    def __init__(self, node1, node2, condition, threshold=0):
+        self.condition = condition
+        self.node1 = node1
+        self.node2 = node2
+        self.threshold = threshold            
+            
+                
+class RuleGraph(object):
 
     def __init__(self):
-        self._predicates = set()
+        self.nodes = []
+        self.edges = []
+        self.start = Node()
+        self.end = Node()
+        
+
+class EdgeStatus(object):
+
+    def __init__(self, edge):
+        self.edge = edge
+        self.count = count
+        
+        
+class GraphStatus(object):
+
+    def __init__(self, graph):
+        self._current_node = graph.start  
+        self._edge_status = None
+        self._context = {}
+        self._start_time = None
+
+    def evaluate(self, event):
+        # check all current edge status
+        # TODO
+        # check all outgoing edges
+        # TODO
+        pass
 
 
-class RuleBinding(object):
 
-    def __init__(self, rule):
-        self._rule = rule 
+# RULE 1
+#  <c1>;
+#  <c2> WITHIN t
+#
+# graph with time goals
+# 
+#   (start)--c1-->( s1 )--c2-->( end, t)
+#
+# graph with expires
+#
+#   (start)--c1-->( s1, t )--c2-->( end )
+#
+# RULE 2
+#  <c1>;
+#  <c2> WITHIN t1;
+#  <c3> WITHIN t2
+#
+# graph with time goals
+#
+#    (start)--c1-->( s1 )--c2->( s2, t1 )--c3-->( end, t1 + t2 )
+#
+# graph with expires
+#    (start)--c1-->( s1, t1 )--c2->( s2, t1 + t2 )--c3-->( end )
+#
+# RULE 3
+#  <c1>;
+#  <c2> OR <c3> WITHIN t
+#
+#  graph with time goals
+#
+#                         / c2 \
+#                        /      \
+#    (start)--c1-->( s1 )        ->( end, t1 + t2 )
+#                        \      /
+#                         \ c3 /
+#
