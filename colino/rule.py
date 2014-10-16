8964f585e3b504451bd801f21eb7edb9ac8aa25c
Gabriele Mambrini <g.mambrini@gmail.com>
2014-10-16 22:32:17 +0200

    updated comments

--- colino/rule.py
+++ colino/rule.py
@@ -48,8 +48,9 @@ class GraphStatus(object):
         # TODO
         pass
 
-
-
+# sample rules and graph translations
+#
+#
 # RULE 1
 #  <c1>;
 #  <c2> WITHIN t
@@ -58,7 +59,8 @@ class GraphStatus(object):
 # 
 #   (start)--c1-->( s1 )--c2-->( end, t)
 #
-# graph with expires
+# graph with expires, i.e. node s1 should be left before t.
+# expire time is the maximum of the time goals of all node successors.
 #
 #   (start)--c1-->( s1, t )--c2-->( end )
 #
@@ -69,10 +71,12 @@ class GraphStatus(object):
 #
 # graph with time goals
 #
-#    (start)--c1-->( s1 )--c2->( s2, t1 )--c3-->( end, t1 + t2 )
+#    ( start )--c1-->( s1 )--c2->( s2, t1 )--c3-->( end, t1 + t2 )
 #
 # graph with expires
-#    (start)--c1-->( s1, t1 )--c2->( s2, t1 + t2 )--c3-->( end )
+#
+#    ( start )--c1-->( s1, t1 )--c2->( s2, t1 + t2 )--c3-->( end )
+#
 #
 # RULE 3
 #  <c1>;
@@ -86,3 +90,8 @@ class GraphStatus(object):
 #                        \      /
 #                         \ c3 /
 #
+#
+# RULE 4
+#  <c1> <n> TIMES WITHIN t
+#
+#    (start)--c1,n-->( end, t )
