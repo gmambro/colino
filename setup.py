555f8a325388f12787a78ff929d943d3de4ed9cf
Gabriele Mambrini <g.mambrini@gmail.com>
2014-10-16 22:05:43 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+from setuptools import setup, find_packages # Always prefer setuptools over distutils
+from codecs import open # To use a consistent encoding
+from os import path
+here = path.abspath(path.dirname(__file__))
 
-from distutils.core import setup
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = "colino",
-    packages = ["colino"],
     version = "0.0.1",
+    
     description = "Event correlator",
+    long_description = long_description,
+
     author = "Gabriele Mambrini",
-    author_email = "g.mambrini@gmail.com"
+    author_email = "g.mambrini@gmail.com",
+
     url = "http://github.com/gmambro/colino",
-#    download_url = "",
-    keywords = ["event" ],
+    # download_url = "",
+
     classifiers = [
         "Programming Language :: Python",
 
@@ -27,7 +37,15 @@ setup(
 	'Programming Language :: Python :: 2.7',
 
         ],
-    long_description = """\
-Colino simple event correlator
-"""
+    license = 'Apache',
+
+    keywords = ["event" ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    entry_points={
+        'console_scripts': [
+             'colino=colino:main',
+        ],
+    },
 )
